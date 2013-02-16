3ac9e83890a939997c3f016c163d9a182b9098b2
igio90 <giovanni.rocca.90@gmail.com>
2013-02-16 14:27:36 +0100

    Settings update 2
    Change-Id: Iaee53533aca5ef8ed8db9805da5ce833ca0a6e52

--- src/com/android/settings/paranoid/Lockscreen.java
+++ src/com/android/settings/paranoid/Lockscreen.java
@@ -56,18 +56,14 @@ public class Lockscreen extends SettingsPreferenceFragment
     private static final int LOCKSCREEN_BACKGROUND_CUSTOM_IMAGE = 1;
     private static final int LOCKSCREEN_BACKGROUND_DEFAULT_WALLPAPER = 2;
 
-    private static final String KEY_ALLOW_ROTATION = "allow_rotation";
     private static final String KEY_SEE_TRHOUGH = "see_through";
     private static final String KEY_HOME_SCREEN_WIDGETS = "home_screen_widgets";
     private static final String KEY_LOCKSCREEN_MAXIMIZE_WIDGETS = "lockscreen_maximize_widgets";
     private static final String KEY_BACKGROUND_PREF = "lockscreen_background";
-    private static final String KEY_ALWAYS_BATTERY_PREF = "lockscreen_battery_status";
     private static final String KEY_LOCKSCREEN_BUTTONS = "lockscreen_buttons";
 
-    private ListPreference mBatteryStatus;
     private ListPreference mCustomBackground;
 
-    private CheckBoxPreference mAllowRotation;
     private CheckBoxPreference mSeeThrough;
     private CheckBoxPreference mHomeScreenWidgets;
     private CheckBoxPreference mMaximizeWidgets;
@@ -88,10 +84,6 @@ public class Lockscreen extends SettingsPreferenceFragment
         addPreferencesFromResource(R.xml.lock_screen_settings);
         PreferenceScreen prefSet = getPreferenceScreen();
         mContext = getActivity();
-
-        mAllowRotation = (CheckBoxPreference) prefSet.findPreference(KEY_ALLOW_ROTATION);
-        mAllowRotation.setChecked(Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.LOCKSCREEN_ALLOW_ROTATION, 0) == 1);
 
         mSeeThrough = (CheckBoxPreference) prefSet.findPreference(KEY_SEE_TRHOUGH);
         mSeeThrough.setChecked(Settings.System.getInt(mContext.getContentResolver(),
@@ -116,18 +108,9 @@ public class Lockscreen extends SettingsPreferenceFragment
         mWallpaperImage = new File(getActivity().getFilesDir() + "/lockwallpaper");
         mWallpaperTemporary = new File(getActivity().getCacheDir() + "/lockwallpaper.tmp");
 
-        mBatteryStatus = (ListPreference) findPreference(KEY_ALWAYS_BATTERY_PREF);
-        if (mBatteryStatus != null) {
-            mBatteryStatus.setOnPreferenceChangeListener(this);
-        }
-
         PreferenceScreen lockscreenButtons = (PreferenceScreen) findPreference(KEY_LOCKSCREEN_BUTTONS);
         if (!hasButtons()) {
             getPreferenceScreen().removePreference(lockscreenButtons);
-        }
-
-        if(Utils.getScreenType(mContext) == Utils.DEVICE_TABLET) {
-            prefSet.removePreference(mAllowRotation);
         }
     }
 
@@ -150,11 +133,7 @@ public class Lockscreen extends SettingsPreferenceFragment
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if (preference == mAllowRotation) {
-            Settings.System.putInt(mContext.getContentResolver(),
-                    Settings.System.LOCKSCREEN_ALLOW_ROTATION, mAllowRotation.isChecked()
-                    ? 1 : 0);
-        } else if (preference == mSeeThrough) {
+        if (preference == mSeeThrough) {
             Settings.System.putInt(mContext.getContentResolver(),
                     Settings.System.LOCKSCREEN_SEE_THROUGH, mSeeThrough.isChecked()
                     ? 1 : 0);
@@ -193,13 +172,6 @@ public class Lockscreen extends SettingsPreferenceFragment
         super.onResume();
 
         ContentResolver cr = getActivity().getContentResolver();
-        if (mBatteryStatus != null) {
-            int batteryStatus = Settings.System.getInt(cr,
-                    Settings.System.LOCKSCREEN_ALWAYS_SHOW_BATTERY, 0);
-            mBatteryStatus.setValueIndex(batteryStatus);
-            mBatteryStatus.setSummary(mBatteryStatus.getEntries()[batteryStatus]);
-        }
-
         if (mMaximizeWidgets != null) {
             mMaximizeWidgets.setChecked(Settings.System.getInt(cr,
                     Settings.System.LOCKSCREEN_MAXIMIZE_WIDGETS, 0) == 1);
@@ -235,13 +207,7 @@ public class Lockscreen extends SettingsPreferenceFragment
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         ContentResolver cr = getActivity().getContentResolver();
 
-        if (preference == mBatteryStatus) {
-            int value = Integer.valueOf((String) objValue);
-            int index = mBatteryStatus.findIndexOfValue((String) objValue);
-            Settings.System.putInt(cr, Settings.System.LOCKSCREEN_ALWAYS_SHOW_BATTERY, value);
-            mBatteryStatus.setSummary(mBatteryStatus.getEntries()[index]);
-            return true;
-        } else if (preference == mMaximizeWidgets) {
+        if (preference == mMaximizeWidgets) {
             boolean value = (Boolean) objValue;
             Settings.System.putInt(cr, Settings.System.LOCKSCREEN_MAXIMIZE_WIDGETS, value ? 1 : 0);
             return true;
